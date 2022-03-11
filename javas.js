ca04145e47d41ffa7b59b754b25d63c134aba81e
Kevin Guier <kevin.guier@gmail.com>
2022-03-11 12:17:58 -0600

    Add the subtraction function
    Using the case select method to determine what operator sign is
    being used

--- javas.js
+++ javas.js
@@ -10,37 +10,85 @@ const btnSub = document.querySelector('subtract');
 
 let num1 = 0;
 let num2 = 0;
+let sign1 = '';
+let sign2 = '';
+display.textContent = undefined;
 
 
 function addToScreen(x){
+    if(display.textContent == num1){
+        display.textContent = x;
+    }else{
+    
     display.textContent += x;
+    }
 }
+         
+    
+ 
 
 function clearAll(){
     display.textContent = '';
     num1 = 0;
     num2= 0;
+    sign = '';
 }
 
-function storeNum(){
+function add(){
     if (num1 == 0){
-        num1 = display.textContent
-    }else{
-        num2 = display.textContent
+        num1 = Number(display.textContent);
+    }else {
+        num2 = Number(display.textContent);
+        num1 = num1 + num2;
+        display.textContent = num1;
+        num2 =0;
     }
-    display.textContent = '';
+    if (sign1 == ''){
+        sign1 = '+';
+    }
+
+}
+
+
+function operate(num1,num2, x){
+    if (x == "+"){
+        add (num1,num2)
+    }else {return;}
 }
 
 function equals(){
-    console.log(num1,num2);
+  if((!num1 == 0) && (display.textContent != 0))
+    num2 = Number(display.textContent);
+        switch(sign1){
+
+            case "+":
+                num1 = num1 +num2;
+                num2 = 0;
+                break;
+            case "-":
+                num1 = num1 - num2;
+                num2 = 0;
+                break;
+                
+            default: num2 = "not works";
+            
+        }
+    
+
+    display.textContent = num1;
+    sign2 = "=";
+    console.log(num1,num2, display.textContent, sign1,sign2);
 }
+    
 
 numKeys.forEach(e => e.addEventListener('click', (btn)=>{addToScreen(btn.target.textContent);}
 ));
 
 btnClear.addEventListener('click',clearAll);
-btnAdd.addEventListener('click', storeNum);
+btnAdd.addEventListener('click', add);
 btnEquals.addEventListener('click',equals);
+
+console.log
 
 
 
